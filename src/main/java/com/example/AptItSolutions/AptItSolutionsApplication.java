4ae91889dd6c237e4f8fd643d87d524112ceea3b
fareed674 <fareedshareef679@gmail.com>
2025-05-14 16:20:13 +0530

    Update AptItSolutionsApplication.java

--- src/main/java/com/example/AptItSolutions/AptItSolutionsApplication.java
+++ src/main/java/com/example/AptItSolutions/AptItSolutionsApplication.java
@@ -1,3 +1,22 @@
+// package com.example;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.builder.SpringApplicationBuilder;
+// import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+// @SpringBootApplication
+// public class AptItSolutionsApplication extends SpringBootServletInitializer {
+
+//     public static void main(String[] args) {
+//         SpringApplication.run(AptItSolutionsApplication.class, args);
+//     }
+
+//     @Override
+//     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//         return application.sources(AptItSolutionsApplication.class);
+//     }
+// }
 package com.example;
 
 import org.springframework.boot.SpringApplication;
@@ -13,7 +32,7 @@ public class AptItSolutionsApplication extends SpringBootServletInitializer {
     }
 
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(AptItSolutionsApplication.class);
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AptItSolutionsApplication.class);
     }
 }
